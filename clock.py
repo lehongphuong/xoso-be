57663ad0f69bd73fd41cf761238c507fa37af84e
PhuongLH5 <helloitpdu@gmail.com>
2019-10-07 09:31:32 +0700

    PhuongLH5: commit all file at 10/07/2019 :  9:31:32.36

--- clock.py
+++ clock.py
@@ -1,10 +1,13 @@
+# https://devcenter.heroku.com/articles/clock-processes-python
+
+
 from scrapinghub import ScrapinghubClient
 import logging 
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler() 
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('cron', hour=9, minutes=35)
 def jobRuning():
     # Enter ScrapingHub
     # Enter ScrapingHub
@@ -17,6 +20,20 @@ def jobRuning():
     spiderID = 'quotes'
     spider = project.spiders.get(spiderID) 
     spider.jobs.run()  
+
+@sched.scheduled_job('cron', hour=9, minutes=40)
+def jobRuning1():
+    # Enter ScrapingHub
+    # Enter ScrapingHub
+    apikey = '40f9881d52794d7bb09b9f5ee6d12a3e'  # your API key as a string
+    client = ScrapinghubClient(apikey)
+    projectID = 410647
+    project = client.get_project(projectID)
+
+    # get spider
+    spiderID = 'quotes'
+    spider = project.spiders.get(spiderID) 
+    spider.jobs.run() 
 
 sched.start()
   
