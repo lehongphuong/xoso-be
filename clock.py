0edfb2cdd25ced736764e9341546daa42567931c
lehongphuong <helloitpdu@gmail.com>
2019-10-07 10:41:21 +0700

    PhuongLH5: commit all file at 10/07/2019 : 10:41:21.08

--- clock.py
+++ clock.py
@@ -7,24 +7,9 @@ import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler() 
  
-@sched.scheduled_job('interval', minutes=1)
-def jobRuning():
-    print(datetime.datetime.now())
-    # Enter ScrapingHub
-    # Enter ScrapingHub
-    apikey = '40f9881d52794d7bb09b9f5ee6d12a3e'  # your API key as a string
-    client = ScrapinghubClient(apikey)
-    projectID = 410647
-    project = client.get_project(projectID)
-
-    # get spider
-    spiderID = 'quotes'
-    spider = project.spiders.get(spiderID) 
-    spider.jobs.run()    
-
-# # @sched.scheduled_job('cron', day_of_week='*', hour=3, minutes=16)
-# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=3, minutes=32)
-# def jobRuning1():
+# @sched.scheduled_job('interval', minutes=1)
+# def jobRuning():
+#     print(datetime.datetime.now())
 #     # Enter ScrapingHub
 #     # Enter ScrapingHub
 #     apikey = '40f9881d52794d7bb09b9f5ee6d12a3e'  # your API key as a string
@@ -35,7 +20,22 @@ def jobRuning():
 #     # get spider
 #     spiderID = 'quotes'
 #     spider = project.spiders.get(spiderID) 
-#     spider.jobs.run()  
+#     spider.jobs.run()    
+
+# @sched.scheduled_job('cron', day_of_week='*', hour=3, minutes=16)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=3, minutes=45)
+def jobRuning1():
+    # Enter ScrapingHub
+    # Enter ScrapingHub
+    apikey = '40f9881d52794d7bb09b9f5ee6d12a3e'  # your API key as a string
+    client = ScrapinghubClient(apikey)
+    projectID = 410647
+    project = client.get_project(projectID)
+
+    # get spider
+    spiderID = 'quotes'
+    spider = project.spiders.get(spiderID) 
+    spider.jobs.run()  
 
 sched.start()
   
