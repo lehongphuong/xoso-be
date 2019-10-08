114c463a4046be8fff37106adb960fb8fb164da5
lehongphuong <helloitpdu@gmail.com>
2019-10-08 18:23:46 +0700

    PhuongLH5: commit all file at 10/08/2019 : 18:23:46.49

--- clock.py
+++ clock.py
@@ -7,7 +7,7 @@ import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler() 
  
-@sched.scheduled_job(trigger='cron', hour='11', minute='30')
+@sched.scheduled_job(trigger='interval', minute='1')
 def jobRuning00():
     # Enter ScrapingHub
     # Enter ScrapingHub
@@ -20,77 +20,7 @@ def jobRuning00():
     spiderID = 'quotes'
     spider = project.spiders.get(spiderID) 
     spider.jobs.run()  
-
-
-@sched.scheduled_job(trigger='cron', hour='11', minute='31')
-def jobRuning01():
-    # Enter ScrapingHub
-    # Enter ScrapingHub
-    apikey = '40f9881d52794d7bb09b9f5ee6d12a3e'  # your API key as a string
-    client = ScrapinghubClient(apikey)
-    projectID = 410647
-    project = client.get_project(projectID)
-
-    # get spider
-    spiderID = 'quotes'
-    spider = project.spiders.get(spiderID) 
-    spider.jobs.run()  
-
-@sched.scheduled_job(trigger='cron', hour='11', minute='32')
-def jobRuning02():
-    # Enter ScrapingHub
-    # Enter ScrapingHub
-    apikey = '40f9881d52794d7bb09b9f5ee6d12a3e'  # your API key as a string
-    client = ScrapinghubClient(apikey)
-    projectID = 410647
-    project = client.get_project(projectID)
-
-    # get spider
-    spiderID = 'quotes'
-    spider = project.spiders.get(spiderID) 
-    spider.jobs.run()  
-
-@sched.scheduled_job(trigger='cron', hour='11', minute='33')
-def jobRuning03():
-    # Enter ScrapingHub
-    # Enter ScrapingHub
-    apikey = '40f9881d52794d7bb09b9f5ee6d12a3e'  # your API key as a string
-    client = ScrapinghubClient(apikey)
-    projectID = 410647
-    project = client.get_project(projectID)
-
-    # get spider
-    spiderID = 'quotes'
-    spider = project.spiders.get(spiderID) 
-    spider.jobs.run()  
-
-@sched.scheduled_job(trigger='cron', hour='11', minute='34')
-def jobRuning04():
-    # Enter ScrapingHub
-    # Enter ScrapingHub
-    apikey = '40f9881d52794d7bb09b9f5ee6d12a3e'  # your API key as a string
-    client = ScrapinghubClient(apikey)
-    projectID = 410647
-    project = client.get_project(projectID)
-
-    # get spider
-    spiderID = 'quotes'
-    spider = project.spiders.get(spiderID) 
-    spider.jobs.run()  
-
-@sched.scheduled_job(trigger='cron', hour='11', minute='35')
-def jobRuning05():
-    # Enter ScrapingHub
-    # Enter ScrapingHub
-    apikey = '40f9881d52794d7bb09b9f5ee6d12a3e'  # your API key as a string
-    client = ScrapinghubClient(apikey)
-    projectID = 410647
-    project = client.get_project(projectID)
-
-    # get spider
-    spiderID = 'quotes'
-    spider = project.spiders.get(spiderID) 
-    spider.jobs.run()  
+ 
 
 sched.start()
   
